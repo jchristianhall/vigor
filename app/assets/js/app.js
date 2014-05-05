a116a777a181ff87f01af74c1a845dc4ec92aaea
Steven Calhoun <stevenacalhoun@gmail.com>
2014-05-05 15:01:51 -0500

    Toying with Chart.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -43,4 +43,103 @@ $(document).ready(function () {
     $('#settings').fadeIn();
     $('.overlay').fadeIn();
   });
+
+var options = {
+        
+  //Boolean - If we show the scale above the chart data     
+  scaleOverlay : false,
+  
+  //Boolean - If we want to override with a hard coded scale
+  scaleOverride : true,
+  
+  //** Required if scaleOverride is true **
+  //Number - The number of steps in a hard coded scale
+  scaleSteps : 4,
+  //Number - The value jump in the hard coded scale
+  scaleStepWidth : 5,
+  //Number - The scale starting value
+  scaleStartValue : 120,
+
+  //String - Colour of the scale line 
+  scaleLineColor : "rgba(0,0,0,.1)",
+  
+  //Number - Pixel width of the scale line  
+  scaleLineWidth : 1,
+
+  //Boolean - Whether to show labels on the scale 
+  scaleShowLabels : true,
+  
+  //Interpolated JS string - can access value
+  scaleLabel : "<%=value%>",
+  
+  //String - Scale label font declaration for the scale label
+  scaleFontFamily : "'Lato'",
+  
+  //Number - Scale label font size in pixels  
+  scaleFontSize : 12,
+  
+  //String - Scale label font weight style  
+  scaleFontStyle : "normal",
+  
+  //String - Scale label font colour  
+  scaleFontColor : "rgba(52, 73, 94, 1)",  
+  
+  ///Boolean - Whether grid lines are shown across the chart
+  scaleShowGridLines : true,
+  
+  //String - Colour of the grid lines
+  scaleGridLineColor : "rgba(0,0,0,.05)",
+  
+  //Number - Width of the grid lines
+  scaleGridLineWidth : 1, 
+  
+  //Boolean - Whether the line is curved between points
+  bezierCurve : false,
+  
+  //Boolean - Whether to show a dot for each point
+  pointDot : true,
+  
+  //Number - Radius of each point dot in pixels
+  pointDotRadius : 3,
+  
+  //Number - Pixel width of point dot stroke
+  pointDotStrokeWidth : 1,
+  
+  //Boolean - Whether to show a stroke for datasets
+  datasetStroke : true,
+  
+  //Number - Pixel width of dataset stroke
+  datasetStrokeWidth : 2,
+  
+  //Boolean - Whether to fill the dataset with a colour
+  datasetFill : true,
+  
+  //Boolean - Whether to animate the chart
+  animation : true,
+
+  //Number - Number of animation steps
+  animationSteps : 60,
+  
+  //String - Animation easing effect
+  animationEasing : "easeOutQuart",
+
+  //Function - Fires when the animation is complete
+  onAnimationComplete : null
+  
+}
+  var data = {
+    labels : ["3/8","3/9","3/10","3/11","3/12","3/13","3/14"],
+    datasets : [{
+      fillColor : "rgba(220,220,220,0.5)",
+      strokeColor : "rgba(231, 139, 67, 1)",
+      pointColor : "rgba(231, 139, 67, 1)",
+      pointStrokeColor : "rgba(231, 139, 67, 1)",
+      data : [130,135,131,132,130,140,135]
+    }]
+  }
+
+  var ctx = $("#weight-graph").get(0).getContext("2d");
+  var weightChart = new Chart(ctx);
+
+  new Chart(ctx).Line(data,options);
 });
